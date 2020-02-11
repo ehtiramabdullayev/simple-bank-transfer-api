05518464a11ff8aac3663426fb21eceee0766594
master <ehtiramabdullayev@gmail.com>
2020-02-11 23:48:17 +0100

    change the scope of injection

--- src/main/java/com/bank/transfer/module/ApplicationModule.java
+++ src/main/java/com/bank/transfer/module/ApplicationModule.java
@@ -11,6 +11,7 @@ import com.bank.transfer.service.impl.AccountServiceImpl;
 import com.bank.transfer.service.impl.JsonParserImpl;
 import com.bank.transfer.service.impl.TransferServiceImpl;
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
 
 /**
  * @author Ehtiram_Abdullayev on 2/9/2020
@@ -19,10 +20,13 @@ import com.google.inject.AbstractModule;
 public class ApplicationModule extends AbstractModule {
     @Override
     protected void configure() {
-        bind(AccountRepository.class).to(AccountRepositoryImpl.class);
-        bind(AccountService.class).to(AccountServiceImpl.class);
-        bind(TransferService.class).to(TransferServiceImpl.class);
-        bind(OperationsController.class).to(OperationsControllerImpl.class);
-        bind(JsonParser.class).to(JsonParserImpl.class);
+        /* in here i wanted to guarantee Guice will not create a new instance of dependencies for each injection,
+           otherwise we might end up with multiple in memory dbs
+        */
+        bind(AccountRepository.class).to(AccountRepositoryImpl.class).in(Singleton.class);
+        bind(AccountService.class).to(AccountServiceImpl.class).in(Singleton.class);
+        bind(TransferService.class).to(TransferServiceImpl.class).in(Singleton.class);
+        bind(OperationsController.class).to(OperationsControllerImpl.class).in(Singleton.class);
+        bind(JsonParser.class).to(JsonParserImpl.class).in(Singleton.class);
     }
 }
